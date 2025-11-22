1008d9e83302f491645c91e242b11fca8fa3cd28
Roromo0306 <pownyrainbow03@gmail.com>
2025-11-22 17:32:14 +0100

    Reset Proyecto

--- 2DInfiniteRunner_Mecanicas/Assets/Scripts/Strategies/IObstacleSpawnStategy.cs
+++
@@ -1,7 +0,0 @@
-public interface IObstacleSpawnStrategy
-{
-    // devuelve intervalo hasta siguiente spawn
-    float GetNextSpawnInterval(float gameTime, float baseInterval);
-    // devuelve parámetros del obstáculo (por simplicidad solo width y speed)
-    (float width, float speed) GetObstacleParams(float gameTime);
-}
--- 2DInfiniteRunner_Mecanicas/Assets/Scripts/Strategies/IPowerUpEffectStrategy.cs
+++
@@ -1,6 +0,0 @@
-using RunnerGame.MVC.Model;
-public interface IPowerUpEffectStrategy
-{
-    void Apply(PlayerModel player, float duration);
-    void Remove(PlayerModel player);
-}
